5a02946c534ee2dda650619603f5cedd446a5826
Dmitry <dredbads@gmail.com>
2021-03-02 23:25:48 +0500

    style fix

--- assets/site/js/yearConfirm.js
+++ assets/site/js/yearConfirm.js
@@ -1,6 +1,3 @@
-const overlay = document.querySelector('.year-confirm-overlay')
-const confirmBlock = document.querySelector('.year-conform')
-
 function createModal() {
   const modal = document.createElement('div')
   modal.classList.add('ymodal')
@@ -8,7 +5,7 @@ function createModal() {
     <div class="year-confirm-overlay">
       <div class="year-confirm"><img src="assets/site/images/year-im.svg" alt="">
         <p class="year-p">Сайт содержит информацию, которая не рекомендована лицам, не достигшим совершеннолетия. Для входа на сайт, подтвердите свой возраст.</p>
-        <a href="#" class="button bg-red w-inline-block">
+        <a href="#" class="button bg-red" id="close-year-modal">
           <div>Подтверждаю, мне есть 18</div>
           <div class="button-border _1"></div>
           <div class="button-border _2"></div>
@@ -21,8 +18,30 @@ function createModal() {
   document.body.appendChild(modal)
   return modal
 }
-const $modal = createModal()
 
-document.body.addEventListener('DOMContentLoaded', () => {
-  $modal.classList.add('open')
-})
+const $modal = createModal()
+window.addEventListener('load', openModal)
+
+function openModal() {
+  const loaded = localStorage.getItem('year-confirm')
+  if (!loaded) {
+    $modal.classList.add('open')
+    document.body.style.overflow = 'hidden'
+  }
+}
+
+// closeModal
+
+const closeModal = document.getElementById('close-year-modal')
+closeModal.addEventListener('click', handlerModal)
+
+function handlerModal() {
+  $modal.classList.add('hidden')
+  setTimeout(() => {
+    $modal.classList.remove('open')
+  }, 300)
+  document.body.style.overflow = 'visible'
+  localStorage.setItem('year-confirm', JSON.stringify(true))
+}
+
+Webflow.require('ix2').init()
