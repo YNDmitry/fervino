5c147b14a760387dd33e1ef4cd362b822275083d
Dmitry <dredbads@gmail.com>
2021-03-08 20:55:53 +0500

    styles

--- assets/site/js/preloader.js
+++ assets/site/js/preloader.js
@@ -1,9 +1,24 @@
-const loader = document.querySelector('.preloader')
+function init() {
+  const loader = document.createElement('div')
+  loader.classList.add('preloader')
+  loader.classList.add('open')
+  loader.insertAdjacentHTML('afterBegin', `
+    <div class="loader-content">
+      <img src="assets/site/images/logo-1.png">
+      <div class="loader-vec"></div>
+    </div>
+    <div class="loader-txt"></div>
+  `)
+  document.body.appendChild(loader)
+  return loader
+}
+let initLoader = init()
 
 window.addEventListener('load', handleLoad)
 
 function handleLoad() {
   setTimeout(() => {
-    loader.classList.remove('open')
-  }, 200)
+    initLoader.remove()
+  }, 300)
+  destroyed = true
 }
